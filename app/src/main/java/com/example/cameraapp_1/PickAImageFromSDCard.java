5c99dc3eb295ca074b85692f02c75fbf9638f897
furkankapukaya <kapukayafurkan86@gmail.com>
2017-10-30 20:08:06 +0200

    framemerge

--- app/src/main/java/com/example/cameraapp_1/PickAImageFromSDCard.java
+++ app/src/main/java/com/example/cameraapp_1/PickAImageFromSDCard.java
@@ -1,6 +1,7 @@
 package com.example.cameraapp_1;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -9,12 +10,17 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.VideoView;
-
+import org.jcodec.api.android.AndroidSequenceEncoder;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.io.SeekableByteChannel;
+import org.jcodec.common.model.Rational;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
@@ -25,6 +31,7 @@ import org.opencv.core.Core;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -32,6 +39,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+
+import org.bytedeco.javacv.FrameGrabber;
+import org.bytedeco.javacv.*;
 
 import static java.lang.Math.abs;
 
@@ -42,12 +52,19 @@ public class PickAImageFromSDCard extends AppCompatActivity {
         }
         else {Log.e("OPENCV","Not");}
     }
-
+    private Uri pickedURI=null;
     private static final int SELECT_PHOTO = 100;
     ImageView image;
     ImageView video;
-   // VideoView video;
+    VideoView videoPreview;
+    String picturePath=null;
+    public void goPro(View view){
+        Intent i = new Intent(view.getContext(), ProcessActivity.class);
+        startActivity(i);
+    }
 
+
+   // VideoView video;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,9 +79,10 @@ public class PickAImageFromSDCard extends AppCompatActivity {
 
 
     public void pickAImage(View view) {
-        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         photoPickerIntent.setType("*/*");
         startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+        //startActivity(new Intent(PickAImageFromSDCard.this, RecordActivity.class));
     }
 
     @Override
@@ -75,14 +93,32 @@ public class PickAImageFromSDCard extends AppCompatActivity {
             case SELECT_PHOTO:
                 if (resultCode == RESULT_OK) {
                     Uri selectedImage = imageReturnedIntent.getData();
+                    String[] filePathColumn = { MediaStore.Video.Media.DATA };
+
+                    Cursor cursor = getContentResolver().query(selectedImage,
+                            filePathColumn, null, null, null);
+                    cursor.moveToFirst();
+
+                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    picturePath = cursor.getString(columnIndex);
+                    cursor.close();
+
+                    //videoPreview = (VideoView) findViewById(R.id.videoPreview);
+                    Log.e("VIDEOPATH",picturePath);
+                    // videoPreview.setVideoPath(picturePath);
+                    /*Log.e("FILEURI",selectedImage.getPath());
+
                     InputStream imageStream = null;
                     try {
                         imageStream = getContentResolver().openInputStream(selectedImage);
+                        Log.e("imageStream", String.valueOf(imageStream));
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
                     }
                     Bitmap yourSelectedImage = BitmapFactory.decodeStream(imageStream);
-                  //  video.setVideoURI(selectedImage);
+                    Log.e("bitmap", String.valueOf(yourSelectedImage));
+                */
+                    //  video.setVideoURI(selectedImage);
                    // video.start();
                   // image.setImageURI(selectedImage);// To display selected image in image view
                 }
@@ -90,13 +126,22 @@ public class PickAImageFromSDCard extends AppCompatActivity {
     }
 
     public void pickImageWithPath(View view) {
-        File videoFile = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Pictures/Hello_Camera/VID_20171014_134124.mp4");
+        File videoFile = new File(picturePath);
 
+
+
+        if(picturePath != null){
+            videoFile = new File(picturePath);
+        }
+        Log.e("VIDEO", String.valueOf(videoFile));
+        Log.e("VIDEOuri", String.valueOf(videoFile.getAbsolutePath()));
         Uri videoFileUri = Uri.parse(videoFile.toString());
         MediaMetadataRetriever retriever = new MediaMetadataRetriever();
         retriever.setDataSource(videoFile.getAbsolutePath());
         ArrayList<Bitmap> rev = new ArrayList<Bitmap>();
         ArrayList<Bitmap> orig = new ArrayList<Bitmap>();
+        ArrayList<Mat> matList = new ArrayList<Mat>();
+
 
         MediaPlayer mp = MediaPlayer.create(getBaseContext(), videoFileUri);
 
@@ -107,24 +152,25 @@ public class PickAImageFromSDCard extends AppCompatActivity {
         rev.add(bitmap_old);
         // int color1 = bitmap_old.getPixel(25,25);
         int co = 0;
-        for (int i = 1000000; i < millis * 1000; i += 1000000) {
+        for (int i = 300000; i < millis * 1000; i += 300000) {
 
             Bitmap bitmap = retriever.getFrameAtTime(i, MediaMetadataRetriever.OPTION_CLOSEST);
             Bitmap newbit = retriever.getFrameAtTime(i, MediaMetadataRetriever.OPTION_CLOSEST);
-            Mat imgToProcess1 = new Mat(h, w, CvType.CV_8UC4);
-            Mat imgToProcess2 = new Mat(h, w, CvType.CV_8UC4);
+           // Mat imgToProcess1 = new Mat(h, w, CvType.CV_8UC4);
+           // Mat imgToProcess2 = new Mat(h, w, CvType.CV_8UC4);
 
-            Utils.bitmapToMat(newbit, imgToProcess1 );
-            Utils.bitmapToMat(bitmap_old, imgToProcess2);
-            Imgproc.cvtColor(imgToProcess1, imgToProcess1,Imgproc.COLOR_RGB2GRAY);
-            Imgproc.cvtColor(imgToProcess2, imgToProcess2,Imgproc.COLOR_RGB2GRAY);
-            Mat imgTo = new Mat(h,w,CvType.CV_8UC4);
-            Core.absdiff(imgToProcess1,imgToProcess2,imgTo);
-            String folder = Environment.getExternalStorageDirectory().toString();
-            Imgcodecs.imwrite(folder+"/Pictures/focus/"+ String.valueOf(co)+".jpeg",imgTo);
-            co++;
-            for (int j = 0; j < w; j++) {
-
+           //// Utils.bitmapToMat(newbit, imgToProcess1);
+          // Utils.bitmapToMat(bitmap_old, imgToProcess2);
+           // I//mgproc.cvtColor(imgToProcess1, imgToProcess1,Imgproc.COLOR_RGB2GRAY);
+          //  Imgproc.cvtColor(imgToProcess2, imgToProcess2,Imgproc.COLOR_RGB2GRAY);
+           // Mat imgTo = new Mat(h,w,CvType.CV_8UC4);
+            //Core.absdiff(imgToProcess1,imgToProcess2,imgTo);
+            //String folder = Environment.getExternalStorageDirectory().toString();
+            //Imgcodecs.imwrite(folder+"/Pictures/focus/"+ String.valueOf(co)+".jpeg",imgTo);
+           // matList.add(imgTo);
+            rev.add(bitmap);
+            //co++;
+           /* for (int j = 0; j < w; j++) {
                 for (int k = 0; k < h; k++) {
                     int color1 = bitmap_old.getPixel(j, k);
                     int color2 = newbit.getPixel(j, k);
@@ -143,19 +189,19 @@ public class PickAImageFromSDCard extends AppCompatActivity {
                         newbit.setPixel(j, k, Color.WHITE);
                         colorx=color1;
                     }
-                  /*  if(newbit.getPixel(j,k) == Color.GREEN){
-                        newbit.setPixel(j,k,colorx);
-                    }
+                  //  if(newbit.getPixel(j,k) == Color.GREEN){
+                    //    newbit.setPixel(j,k,colorx);
+                   // }
 
-                    color2 = newbit.getPixel(j,k);
-                    red2 = Color.red(color2);
-                    blue2 = Color.blue(color2);
-                    if(abs(red1 - red2) < 5 && abs(blue1-blue2)<5 && abs(green1-green2)<5){
-                        newbit.setPixel(j,k,Color.WHITE);
-                    }*/
+                  //  color2 = newbit.getPixel(j,k);
+                  //  red2 = Color.red(color2);
+                  //  blue2 = Color.blue(color2);
+                   // if(abs(red1 - red2) < 5 && abs(blue1-blue2)<5 && abs(green1-green2)<5){
+                    //    newbit.setPixel(j,k,Color.WHITE);
+                   // }
                 }
-            }
-            rev.add(newbit);
+            }*/
+           // rev.add(newbit);
             bitmap_old = bitmap;
             try {
                 saveFrames(rev);
@@ -165,14 +211,26 @@ public class PickAImageFromSDCard extends AppCompatActivity {
             }
 
         }
+        /*for(int i = 0;i<rev.size();i++){
+            image.setImageBitmap(rev.get(6));
+        }*/
+
 
 
     }
     public void saveFrames(ArrayList<Bitmap> saveBitmapList) throws IOException{
-
+        SeekableByteChannel out = null;
         String folder = Environment.getExternalStorageDirectory().toString();
         File saveFolder = new File(folder + "/Pictures/fr123/");
-        if(!saveFolder.exists()){
+        out = NIOUtils.writableFileChannel(folder + "/Pictures/output.mp4");
+        File sFolder = new File(folder+"/Pictures/output.mp4");
+        AndroidSequenceEncoder encoder = new AndroidSequenceEncoder(out, Rational.R(24,1));
+        for(Bitmap b: saveBitmapList){
+            encoder.encodeImage(b);
+        }
+        encoder.finish();
+
+        /*if(!saveFolder.exists()){
             saveFolder.mkdirs();
         }
 
@@ -192,7 +250,7 @@ public class PickAImageFromSDCard extends AppCompatActivity {
             fo.close();
 
             i++;
-        }
+        }*/
 
     }
 }
--- app/src/main/java/com/example/cameraapp_1/ProcessActivity.java
+++ app/src/main/java/com/example/cameraapp_1/ProcessActivity.java
@@ -1,13 +1,151 @@
 package com.example.cameraapp_1;
 
+import android.graphics.Bitmap;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
-public class ProcessActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+
+
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvException;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.video.BackgroundSubtractorMOG2;
+import org.opencv.videoio.VideoCapture;
+
+import static java.lang.Math.abs;
+
+public class ProcessActivity extends AppCompatActivity implements CvCameraViewListener2{
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("OPENCV TAG ", "opencv yok");
+        }
+    }
+
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat mRgba;
+    private Mat savedGray;
+    private Mat savedRgba;
+    private Mat mBlack;
+    private int H;
+    private int W;
+    private double frame1[];
+    private double frame2[];
+    private double whiteframe[];
+    private double blackframe[];
+    private Mat mGray;
+
+    private boolean isFrameSaved = false;
+
+
+
+    public ProcessActivity() {
+        Log.i("TAGTAAGTAG", "Instantiated new " + this.getClass());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_process);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.tutorial2_activity_surface_view);
+        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        mOpenCvCameraView.enableView();
+
     }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mBlack = new Mat(height, width, CvType.CV_8UC4);
+
+        mGray = new Mat(height, width, CvType.CV_8UC4);
+
+        savedRgba = new Mat(height, width, CvType.CV_8UC4);
+        savedGray = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        H = mRgba.height();
+        W = mRgba.width();
+        if(!isFrameSaved){
+            mGray = inputFrame.rgba();
+            mBlack=inputFrame.rgba();
+            Imgproc.cvtColor(inputFrame.gray(), mGray, Imgproc.COLOR_GRAY2RGBA, 4);
+            Imgproc.cvtColor(inputFrame.gray(), mBlack, Imgproc.COLOR_GRAY2RGBA, 4);
+
+            blackframe = mRgba.get(5,5);
+            whiteframe = mRgba.get(5,5);
+            Log.e("BLACKFRAME", "bblacked");
+            for(int i = 0; i<mRgba.get(5,5).length;i++){
+                blackframe[i] = 0;
+                whiteframe[i] = 255;
+            }
+            Log.e("WHITE", "white");
+            for(int i=0;i<H;i++){
+                for(int j =0; j<W;j++){
+                    mBlack.put(i,j,blackframe);
+                }
+            }
+            mGray = mBlack.clone();
+        }
+        if(isFrameSaved) {
+            Imgproc.cvtColor(inputFrame.gray(), mRgba, Imgproc.COLOR_GRAY2RGBA, 4);
+            Imgproc.cvtColor(savedRgba, savedGray, Imgproc.COLOR_GRAY2RGBA, 4);
+            mGray = mBlack.clone();
+            Core.absdiff(mRgba, savedGray, mRgba);
+            /*for (int i = 100; i < H - 100; i++) {
+                for (int j = 100; j < W - 100; j++) {
+                    boolean farkli = false;
+                    frame1 = mRgba.get(i, j);
+                    frame2 = savedGray.get(i, j);
+                    for (int k = 0; k < frame1.length; k++) {
+                        if (abs(frame1[k] - frame2[k]) > 25) {
+                            farkli = true;
+                            k = frame1.length + 1;
+                        }
+                    }
+                    mRgba.put(i,j, blackframe);
+                    if (farkli) {
+                        mRgba.put(i, j, whiteframe);
+                    }
+
+                }
+            }*/
+
+        }
+       // mRgba = mGray.clone();
+        savedRgba = inputFrame.gray();
+        isFrameSaved = true;
+        return mRgba;
+    }
+
+
 }
